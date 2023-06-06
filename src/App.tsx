e0587aa9ef687eaefa7630c184146be473277e8b
Jacob Fowles <js.fowles@gmail.com>
2023-06-06 13:46:31 -0600

    updated packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import React from 'react';
 import { version } from '../package.json';
 
 const Home = () => (
-  <main className="bg-black text-white flex min-h-screen flex-col gap-32 p-[7vw] items-center">
-    <div className="z-10 w-full flex flex-col space-y-12 items-center justify-between font-mono max-w-prose">
+  <main className="flex min-h-screen flex-col items-center gap-32 bg-black p-[7vw] text-white">
+    <div className="z-10 flex w-full max-w-prose flex-col items-center justify-between space-y-12 font-mono">
       <div className="">
         <a
           className="flex gap-2 "
@@ -13,7 +14,7 @@ const Home = () => (
           <img src="/vectorform.svg" alt="Vectorform Logo" width={90} />
         </a>
       </div>
-      <h1 className="text-4xl">Vite Starter v</h1>
+      <h1 className="text-4xl">Vite Starter v{version}</h1>
       <p>
         Get started by editing{' '}
         <code className="font-mono font-bold">src/App.tsx</code>
