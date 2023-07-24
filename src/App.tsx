14fd5a1b8a87d346a8d7abc771ca6bcfb1ccec98
Jacob Fowles <js.fowles@gmail.com>
2023-07-24 10:11:10 -0600

    updated app

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,9 @@ import { version } from '../package.json';
 const Home = () => (
   <main className="flex min-h-screen flex-col items-center gap-32 bg-black p-[7vw] text-white">
     <div className="z-10 flex w-full max-w-prose flex-col items-center justify-between space-y-12 font-mono">
-      <div className="">
-        <a
-          className="flex gap-2 uppercase"
-          href="https://vectorform.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          by
-          <img src="/vectorform.svg" alt="Vectorform Logo" width={90} />
-        </a>
-      </div>
       <h1 className="text-4xl">Vite Starter v{version}</h1>
       <p>
-        Get started by editing{' '}
+        Get started by editing
         <code className="bg-gray-700 font-mono font-bold">src/App.tsx</code>
       </p>
     </div>
